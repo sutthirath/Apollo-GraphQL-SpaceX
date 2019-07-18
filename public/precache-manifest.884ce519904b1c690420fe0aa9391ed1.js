6bc68091b47b877d048e504007220158edb6b717
Sutthirath Sean Vilaysane <seanvilaysane@gmail.com>
2019-07-18 15:11:53 -0700

    launch page complete

---
+++ public/precache-manifest.884ce519904b1c690420fe0aa9391ed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f95956c4429d58173d729a9c06f8921",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dd90007bd6fafacdd35",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "402dcf8866ee2580cb74",
+    "url": "/static/js/2.44f4bc85.chunk.js"
+  },
+  {
+    "revision": "6dd90007bd6fafacdd35",
+    "url": "/static/js/main.402f9220.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
